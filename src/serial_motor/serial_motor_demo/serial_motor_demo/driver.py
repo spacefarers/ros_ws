43496ec6d3e3d1e017cb1571c32b5a9e53b3827d
spacefarers <michael@yangm.tech>
2023-02-20 19:43:16 -0500

    change e bug

--- src/serial_motor/serial_motor_demo/serial_motor_demo/driver.py
+++ src/serial_motor/serial_motor_demo/serial_motor_demo/driver.py
@@ -9,12 +9,10 @@ import serial
 from threading import Lock
 
 
-
 class MotorDriver(Node):
 
     def __init__(self):
         super().__init__('motor_driver')
-
 
         # Setup parameters
 
@@ -22,26 +20,20 @@ class MotorDriver(Node):
         if (self.get_parameter('encoder_cpr').value == 0):
             print("WARNING! ENCODER CPR SET TO 0!!")
 
-
         self.declare_parameter('loop_rate', value=0)
         if (self.get_parameter('loop_rate').value == 0):
             print("WARNING! LOOP RATE SET TO 0!!")
 
-
         self.declare_parameter('serial_port', value="/dev/ttyUSB0")
         self.serial_port = self.get_parameter('serial_port').value
 
-
         self.declare_parameter('baud_rate', value=57600)
         self.baud_rate = self.get_parameter('baud_rate').value
-
 
         self.declare_parameter('serial_debug', value=False)
         self.debug_serial_cmds = self.get_parameter('serial_debug').value
         if (self.debug_serial_cmds):
             print("Serial debug enabled")
-
-
 
         # Setup topics & services
 
@@ -54,7 +46,6 @@ class MotorDriver(Node):
         self.speed_pub = self.create_publisher(MotorVels, 'motor_vels', 1)
 
         self.encoder_pub = self.create_publisher(EncoderVals, 'encoder_vals', 1)
-        
 
         # Member Variables
 
@@ -66,19 +57,14 @@ class MotorDriver(Node):
 
         self.mutex = Lock()
 
-
         # Open serial comms
 
         print(f"Connecting to port {self.serial_port} at {self.baud_rate}.")
         self.conn = serial.Serial(self.serial_port, self.baud_rate, timeout=1.0)
         print(f"Connected to {self.conn}")
-        
-
-        
-
 
     # Raw serial commands
-    
+
     def send_pwm_motor_command(self, mot_1_pwm, mot_2_pwm):
         self.send_command(f"o {int(mot_1_pwm)} {int(mot_2_pwm)}")
 
@@ -91,7 +77,6 @@ class MotorDriver(Node):
             return [int(raw_enc) for raw_enc in resp.split()]
         return []
 
-
     # More user-friendly functions
 
     def motor_command_callback(self, motor_command):
@@ -99,7 +84,8 @@ class MotorDriver(Node):
             self.send_pwm_motor_command(motor_command.mot_1_req_rad_sec, motor_command.mot_2_req_rad_sec)
         else:
             # counts per loop = req rads/sec X revs/rad X counts/rev X secs/loop 
-            scaler = (1 / (2*math.pi)) * self.get_parameter('encoder_cpr').value * (1 / self.get_parameter('loop_rate').value)
+            scaler = (1 / (2 * math.pi)) * self.get_parameter('encoder_cpr').value * (
+                        1 / self.get_parameter('loop_rate').value)
             mot1_ct_per_loop = motor_command.mot_1_req_rad_sec * scaler
             mot2_ct_per_loop = motor_command.mot_2_req_rad_sec * scaler
             self.send_feedback_motor_command(mot1_ct_per_loop, mot2_ct_per_loop)
@@ -107,7 +93,6 @@ class MotorDriver(Node):
     def check_encoders(self):
         resp = self.send_encoder_read_command()
         if (resp):
-
             new_time = time.time()
             time_diff = new_time - self.last_enc_read_time
             self.last_enc_read_time = new_time
@@ -117,9 +102,9 @@ class MotorDriver(Node):
             m2_diff = resp[1] - self.last_m2_enc
             self.last_m2_enc = resp[1]
 
-            rads_per_ct = 2*math.pi/self.get_parameter('encoder_cpr').value
-            self.m1_spd = m1_diff*rads_per_ct/time_diff
-            self.m2_spd = m2_diff*rads_per_ct/time_diff
+            rads_per_ct = 2 * math.pi / self.get_parameter('encoder_cpr').value
+            self.m1_spd = m1_diff * rads_per_ct / time_diff
+            self.m2_spd = m2_diff * rads_per_ct / time_diff
 
             spd_msg = MotorVels()
             spd_msg.mot_1_rad_sec = self.m1_spd
@@ -131,12 +116,10 @@ class MotorDriver(Node):
             enc_msg.mot_2_enc_val = self.last_m2_enc
             self.encoder_pub.publish(enc_msg)
 
-
-
     # Utility functions
 
     def send_command(self, cmd_string):
-        
+
         self.mutex.acquire()
         try:
             cmd_string += "\r"
@@ -152,7 +135,7 @@ class MotorDriver(Node):
                 if (c == ''):
                     print("Error: Serial timeout on command: " + cmd_string)
                     return ''
-                else:
+                elif cmd_string != "e":
                     print("Executed Command: " + cmd_string)
                 value += c
 
@@ -168,9 +151,7 @@ class MotorDriver(Node):
         self.conn.close()
 
 
-
 def main(args=None):
-    
     rclpy.init(args=args)
 
     motor_driver = MotorDriver()
@@ -180,9 +161,6 @@ def main(args=None):
         rclpy.spin_once(motor_driver)
         motor_driver.check_encoders()
 
-
     motor_driver.close_conn()
     motor_driver.destroy_node()
     rclpy.shutdown()
-
-
